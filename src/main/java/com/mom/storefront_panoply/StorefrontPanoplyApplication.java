dcbf18627dd854d6b3672b2595ec61f12079aed0
Panagiotis <pgeo82@gmail.com>
2026-03-01 18:06:17 +0200

    add slf4j in the main of the spring boot

--- src/main/java/com/mom/storefront_panoply/StorefrontPanoplyApplication.java
+++ src/main/java/com/mom/storefront_panoply/StorefrontPanoplyApplication.java
@@ -1,10 +1,12 @@
 package com.mom.storefront_panoply;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Slf4j
 @SpringBootApplication
 @EnableFeignClients
 @EnableScheduling
@@ -12,6 +14,6 @@ public class StorefrontPanoplyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StorefrontPanoplyApplication.class, args);
+        log.info("Spring Boot started - log test");
     }
-
 }
