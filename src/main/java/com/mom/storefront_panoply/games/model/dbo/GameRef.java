c5e73d3543c64e06aa19cb6ce1088adf49a99076
Panagiotis <pgeo82@gmail.com>
2026-04-10 21:20:06 +0300

    change the hello controller

--- src/main/java/com/mom/storefront_panoply/games/model/dbo/GameRef.java
+++ src/main/java/com/mom/storefront_panoply/games/model/dbo/GameRef.java
@@ -14,7 +14,8 @@ import org.springframework.data.mongodb.core.mapping.Field;
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class GameRef {
-    @Field("_id")
+    // todo you need to change it in the db the id to be _id for the next gamee
+    @Field("id")
     private String id;
     private String name;
 }
